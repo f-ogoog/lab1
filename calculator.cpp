b3f0b9a3cb6ad5ae4c18c81c68b4f388fd9b4f94
Oleh <zxcnmqqe@gmail.com>
2024-10-11 14:48:49 +0200

    add some code

--- calculator.cpp
+++ calculator.cpp
@@ -2,15 +2,35 @@
 
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+    double sum = a + b;
+
+    if (sum > 1) {
+       return sum 0.5;
+    }
+
+	return sum;
 }
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+    double sub = Add (a, -b);
+
+    if (sub > 1) {
+           return sub 0.5;
+        }
+
+    return sub;
 }
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    double mul = a * b;
+
+    if (mul > 1) {
+           return a * b + 0.5;
+    }
+
+    return a * b;
+
+    std::cout << "cement 2024";
 }
